6b6a561cd2bfd07970e92167c7b1103206c2684b
sanjar <aripovs@internet.ru>
2021-11-29 09:35:17 +0500

    git@github.com:sanjar1987/js5.git

---
+++ homework.js
@@ -0,0 +1,42 @@
+// var num = +prompt('diametr soni')
+// line = ''
+// back = 0
+// for (let x = 0; x < num; x++) {
+//     for (let y = num; y > back; y--) {
+//         if (x == y-1 || x==0|| y==num || y==1|| x==num-1) {
+//             line += '⬜'
+//         }
+//         else {
+//             line += '⬛'
+//         }
+//     }
+//     console.log(line);
+//     line = ''
+// }
+
+
+// let x = 10
+// let num = 1
+// for (let i = 1; i < x + 1; i++) {
+//     num = i
+//     a = prompt('ismingizni kiriting', 'name')
+//     while (!isNaN(a)) {
+//         a = prompt('ismingizni kiriting', "yo'q ismingizni kiriting")
+//     }
+//     b = +prompt('yoshingizni kiriting', 'age')
+//     while (isNaN(b)) {
+//         b = +prompt('yoshingizni kiriting', "yo'q yoshingizni kiriting")
+//     }
+//     let y = 2021
+//     let c = y - b
+//     let user = {
+//         name: a
+//     }
+//     let user2 = {
+//         age: b
+//     }
+//     console.log("Foydalanuvchi " + num);
+//     console.log("    Sizning ismingiz " + user.name);
+//     console.log("    Yoshingiz " + user2.age);
+//     console.log("    Tug'ilgan yilingiz esa " + c);
+// }  
---
+++ obj.js
@@ -0,0 +1,48 @@
+function cartObj() {
+  let rand = randomNumbers(1, 2)
+
+  const priceList = {
+    1: {
+      'Lavash': {
+        info: 'big cheese',
+        price: 15000
+      },
+      'Hot-dog': {
+        info: 'mini',
+        price: 15000
+      },
+      'Cola': {
+        info: '1.5 L',
+        price: 9000
+      },
+    },
+    2: {
+      'Sushi': {
+        info: 'max-roll',
+        price: 32000
+      },
+      'Soya': {
+        info: '200ml',
+        price: 5000
+      },
+    }
+  }
+  let dostavka = 9000
+  for (const price in priceList) {
+    if (rand == 1) {
+      totalPrice = priceList[1]['Lavash'].price + priceList[1]['Hot-dog'].price + priceList[1]['Cola'].price + dostavka
+      console.log("Sizning buyurmangiz: Lavash Big-cheese, Hot-dog mini, Cola 1.5L. Yetkazib berish bilan bo'lgan narx " + totalPrice + " | Yetkazish narxi (9000)");
+    }
+    else if (rand == 2) {
+      totalPrice = priceList[2]['Sushi'].price + priceList[2]['Soya'].price + dostavka
+      console.log("Sizning buyurmangiz: Sushi max-roll, Soya 200ml. Yetkazib berish bilan bo'lgan narx " +totalPrice+ " | Yetkazish narxi (9000)");
+
+    }
+  }
+  return priceList[rand]
+}
+
+function randomNumbers(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+console.log(cartObj());
